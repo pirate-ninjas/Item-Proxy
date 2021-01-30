6cc31381c86f14d94b3689da9d34c7462ed2268f
Kris Rawson <krisrawson@Rawsons-iMac.local>
2021-01-30 03:27:36 -0700

    Installed compression for server.

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
+var compression = require('compression')
+
 
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
 
+app.use(compression());
 app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
 app.use(express.json());
